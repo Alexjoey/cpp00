9f031e8d702558867e56dbe8130c6cbecbae7811
Alex Mylle <alexm@live.be>
2024-11-05 14:59:25 +0100

    added namespace

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -19,6 +19,6 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else for(int i = 1; argv[i]; i++)
 		for(int j = 0; argv[i][j]; j++)
-			std::cout << (char)toupper(argv[i][j]);
+			std::cout << (char)std::toupper(argv[i][j]);
 	std::cout << "\n";
 }
